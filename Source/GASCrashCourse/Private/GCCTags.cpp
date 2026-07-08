8147d8a23ae26254368143c1c1cd38a093bbfd4e
Joao Ricardo Araujo Granzoto <joaoricardo.granzoto@gmail.com>
2026-07-08 17:55:43 -0300

    Adding gameplay tag

---
+++ Source/GASCrashCourse/Private/GCCTags.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GCCTags.h"
+
+namespace GCCTags
+{
+	namespace GCCAbilities
+	{
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(Primary, "GCCTags.GCCAbilities.Primary", "Tag for the primary ability")
+	}
+}
